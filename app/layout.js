f89d0d71128b7f3b37304526bcca051119a9e3ea
thanhKasper <kieutienthanh207@gmail.com>
2024-03-25 22:07:07 +0700

    add gtag next

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
+'use client'
 import "./globals.css";
 import { Providers } from "./providers";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export const metadata = {
   title: "L&F",
@@ -11,14 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TLT11GWHTW"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-TLT11GWHTW');
-      </script>
+      <GoogleTagManager gtmId="G-TLT11GWHTW" />
       <body>
         <AppRouterCacheProvider>
           <Providers>{children}</Providers>
